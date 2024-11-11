ba0f1d550787948a2f2550d34144b4880f9db3f9
RAHUL-KUMAR\Rahul Kumar <rkrahulkmr353@gmail.com>
2024-11-11 15:18:01 +0530

    maven project setup with spring boot

---
+++ src/main/java/com/rahul/asht/AshtApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.asht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahul/asht/AshtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.asht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AshtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
